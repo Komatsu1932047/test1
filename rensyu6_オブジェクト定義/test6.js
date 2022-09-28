6196244a027fdfc45546b32283f98db7c6493f2a
Komatsu1932047 <s1932047qp@s.chibakoudai.jp>
2022-09-28 18:00:26 +0900

    おk

---
+++ rensyu6_オブジェクト定義/test6.js
@@ -0,0 +1,25 @@
+var ufo;
+function init(){
+    window.addEventListener("keydown", keydown);
+    ufo = new UFO(document.getElementById("ufo"), 100);
+    function keydown(e){
+        if(e.keyCode == 37){
+            ufo.moveLeft();
+        }else if(e.keyCode == 39){
+            ufo.moveRight();
+        }
+    }
+    function UFO(_elem, _xpos){
+        this.elem = _elem;
+        this.xpos = _xpos;
+        this.moveLeft = function(){
+            this.xpos -= 10;
+            this.elem.style.left = this.xpos + "px";
+        }
+        this.moveRight = function(){
+            this.xpos += 10;
+            this.elem.style.left = this.xpos + "px"
+        }
+        this.elem.style.left = this.xpos + "px"
+    }
+}
