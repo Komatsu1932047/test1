c818ca5e447bf82dc25b876c950e41739bbaaf4a
{Komatsu1932047} <{komatsuzaki1932047@gmail.com}>
2022-10-04 21:29:45 +0900

    ok

--- rensyu2_for/test2.js
+++
@@ -1,9 +0,0 @@
-function sum(){
-    var v = document.getElementById("number").value;
-    var r = 0;
-    for(var i = 1; i <= v; i++){
-        r += i;
-    }
-    var s = "１から" + v + "の合計は" + r + "です";
-    document.getElementById("result").textContent =s;
-}
