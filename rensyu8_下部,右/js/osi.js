6db7720b0b508e6f52ad07371e3059aa9d8f396e
{Komatsu1932047} <{komatsuzaki1932047@gmail.com}>
2022-10-11 22:33:39 +0900

    ok

--- rensyu8_下部,右/js/osi.js
+++ rensyu8_下部,右/js/osi.js
@@ -1,3 +1,9 @@
+//右部Offcanvas
+document.querySelector('.offcanvas-on').onclick = function () {
+  event.preventDefault()
+  new bootstrap.Offcanvas('#exampleOffcanvas').show();
+};
+//下部タブの操作
 const triggerTabList = document.querySelectorAll('#myTab button')
 triggerTabList.forEach(triggerEl => {
   const tabTrigger = new bootstrap.Tab(triggerEl)
