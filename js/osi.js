abbf81098e9dbf77b4868ff5e2b5c5ce5a57262d
{Komatsu1932047} <{komatsuzaki1932047@gmail.com}>
2022-10-18 01:15:56 +0900

    ok

--- js/osi.js
+++ js/osi.js
@@ -7,7 +7,6 @@ document.querySelector('.offcanvas-on').onclick = function () {
 const triggerTabList = document.querySelectorAll('#myTab button')
 triggerTabList.forEach(triggerEl => {
   const tabTrigger = new bootstrap.Tab(triggerEl)
-
   triggerEl.addEventListener('click', event => {
     event.preventDefault()
     tabTrigger.show()
