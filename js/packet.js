bbd8a393e6cbdeb84cb7ec6b9291aaf370997d10
{Komatsu1932047} <{komatsuzaki1932047@gmail.com}>
2022-10-25 23:41:24 +0900

    ok

---
+++ js/packet.js
@@ -0,0 +1,14 @@
+//右部Offcanvas
+document.querySelector('.offcanvas-on').onclick = function () {
+    new bootstrap.Offcanvas('#exampleOffcanvas').show();
+  };
+  //下部タブの操作
+  const triggerTabList = document.querySelectorAll('#myTab button')
+  triggerTabList.forEach(triggerEl => {
+    const tabTrigger = new bootstrap.Tab(triggerEl)
+    triggerEl.addEventListener('click', event => {
+      event.preventDefault()
+      tabTrigger.show()
+    })
+  })
+  
